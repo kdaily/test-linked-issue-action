d921f7c661ebdc8b5b9c4dc0c7ede8810a9b2e08
Kenneth Daily <kdaily@amazon.com>
2022-05-11 08:47:10 -0700

    Update main.py

--- main.py
+++ main.py
@@ -71,7 +71,7 @@ def main():
 
     my_input_token = os.environ["INPUT_GITHUB_TOKEN"]
     my_input_owner = os.environ["INPUT_OWNER"]
-    my_input_repo = os.environ["INPUT_OWNER"]
+    my_input_repo = os.environ["INPUT_REPO"]
     my_input_number = int(os.environ["INPUT_NUMBER"])
 
     headers={'Authorization': f'bearer {os.environ["INPUT_GITHUB_TOKEN"]}',
